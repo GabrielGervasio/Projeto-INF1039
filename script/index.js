88832890f75ea57fe89c0628556ea79a8c708bd1
Gabriel Gervasio de Santana <114101356+GabrielGervasio@users.noreply.github.com>
2023-10-01 18:49:36 -0300

    Arquivos base para o projeto

---
+++ script/index.js
@@ -0,0 +1,6 @@
+// Este é apenas um exemplo básico de JavaScript
+document.addEventListener('DOMContentLoaded', function () {
+    // Código JavaScript aqui, por exemplo:
+    console.log('O JavaScript está funcionando!');
+    // Você pode adicionar mais lógica aqui conforme necessário
+});
